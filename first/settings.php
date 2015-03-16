001c8733fe383410ad35951b27804229bc3ddb3e
neswill1 <nenadkonatar@live.ac.me>
2015-03-16 15:19:32 +0100

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="5926 Premier Way Naples, FL 34109"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2395947055;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
